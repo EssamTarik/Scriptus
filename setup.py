efa00477595cf99df9995ea1a406dd2373586b72
Essam Tarik <essamtarik2000@gmail.com>
2015-08-19 14:58:57 +0200

    added small setup script to organize the file structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+import shutil,os
+
+Sdir='Scriptus'
+if not os.path.exists(Sdir):
+	os.mkdir(Sdir)
+tomove=['__init__.py','session.py','wrappers.py']
+toremove=['__init__.pyc','session.pyc','wrappers.pyc']
+
+for f in tomove:
+	try:
+		shutil.move(f,os.path.join(Sdir,f))
+	except IOError,error:
+		print str(error)
+
+for f in toremove:
+	try:
+		os.remove(f)
+	except IOError,error:
+		print str(error)
+
+print 'setup complete'
